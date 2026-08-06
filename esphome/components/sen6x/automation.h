a2f714d06d972b5d58f29ff4d2791057e83cbad7
Archer36 <archer36@gmail.com>
2026-08-06 16:14:59 -0400

    [sen6x] Fix fan cleaning action constructor

--- esphome/components/sen6x/automation.h
+++ esphome/components/sen6x/automation.h
@@ -8,6 +8,8 @@ namespace esphome::sen6x {
 
 template<typename... Ts> class StartFanAction final : public Action<Ts...>, public Parented<SEN6XComponent> {
  public:
+  explicit StartFanAction(SEN6XComponent *parent) { this->parent_ = parent; }
+
   void play(const Ts &...x) override { this->parent_->start_fan_cleaning(); }
 };
 
